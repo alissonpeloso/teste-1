0d8fd4a026552074ea2ed3796e254fa346eb36ca
Fernando Bevilacqua <dovyski@gmail.com>
2020-09-23 14:34:38 -0300

    Melhora mensagem de oi

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 class Main {
     public static void main(String args[]) {
         // Oi, tudo bem? Quer tc?
-        System.out.println("Hello world!");
+        System.out.println("Olá world!");
         System.out.println("Oi!");
     }
 }
